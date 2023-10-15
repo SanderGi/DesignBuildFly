51fbcca0cac675db1aa47eaabb065071855e2457
Glitch (glitch-blank-node) <none>
2023-10-15 07:49:08 +0000

    📚🌇 Checkpoint ./server.js:12528664/572 ./public/index.html:12528664/541

--- server.js
+++ server.js
@@ -17,15 +17,25 @@ app.post("/upload", function (req, res) {
     // Logging uploading file 
     console.log(uploadedFile); 
     
-    exec('cat *.js bad_file | wc -l', (error, stdout, stderr) => {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
+    exec('blackbox-tools/blackbox_decode ' + uploadedFile.tempFilePath, (error, stdout, stderr) => {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+        res.sendStatus(500);
+        return;
+      }
+      
+      res.download(uploadedFile.tempFilePath + ".01.csv", function (err) { 
+        if (err) { 
+          console.log(err); 
+        } 
+      }); 
     });
-    
-  } else res.send("No file uploaded !!"); 
+  } else {
+    res.status = 400;
+    res.send("No file uploaded !!");
+  }
 }); 
 
 app.listen(process.env.PORT, () => {
