bfa1233fd023590158102ea3127d2f750b073594
Glitch (glitch-blank-node) <none>
2023-10-15 07:25:04 +0000

    🚊🚴 Checkpoint ./public/index.html:12528664/916 ./package.json:12528664/921 ./server.js:12528664/1309

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
+const fileUpload = require("express-fileupload"); 
 
 app.use(express.static('public'));
+
+app.use(fileUpload({
+  useTempFiles : true,
+  tempFileDir : '/tmp/'
+}));
+
+app.post("/upload", function (req, res) { 
+  if (req.files && Object.keys(req.files).length !== 0) { 
+    const uploadedFile = req.files.uploadFile; 
+  
+    // Logging uploading file 
+    console.log(uploadedFile); 
+    
+  } else res.send("No file uploaded !!"); 
+}); 
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
