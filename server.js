8f3f9ed79a6382bf199b8a4a090edbc95ea7d442
Glitch (alex-dbf) <none>
2023-10-22 23:54:07 +0000

    Rewound to commit 1608e887a500aa73db054b3cf31aac8229480729 + 0 edits

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(fileUpload({
   tempFileDir : '/tmp/'
 }));
 
-app.post("/decode_blackbox", (req, res) => { 
+app.post("/upload", function (req, res) { 
   if (req.files && Object.keys(req.files).length !== 0) { 
     const uploadedFile = req.files.uploadFile; 
   
@@ -34,37 +34,9 @@ app.post("/decode_blackbox", (req, res) => {
     });
   } else {
     res.status = 400;
-    res.send("No file uploaded!!");
+    res.send("No file uploaded !!");
   }
 }); 
-
-app.post("/decode_thrust", (req, res) => {
-  if (req.files && Object.keys(req.files).length !== 0) {
-    const uploadedFile = req.files.uploadFile;
-
-    // Logging uploading file
-    console.log(uploadedFile);
-
-    exec('python3 -m python_tools.scripts.decode_thrust ' + uploadedFile.tempFilePath + ' ' + uploadedFile.tempFilePath + "_csvs.zip", (error, stdout, stderr) => {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
-      if (error !== null) {
-        console.log('exec error: ' + error);
-        res.sendStatus(500);
-        return;
-      }
-
-      res.download(uploadedFile.tempFilePath + "_csvs.zip", function (err) {
-        if (err) {
-          console.log(err);
-        }
-      });
-    });
-  } else {
-    res.status = 400;
-    res.send("No file uploaded!!");
-  }
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
