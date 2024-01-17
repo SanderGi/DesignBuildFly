ac63e1b0e59d069ee696be1c88f75b95189c0850
Alexander Metzger <alexander.le@outlook.dk>
2024-01-17 02:43:16 -0800

    windtunnel data parsing

--- python_tools/data/windTunnel/upload.bat
+++
@@ -1,6 +0,0 @@
-xcopy . ..\DesignBuildFly\python_tools\data\windTunnel /e /y
-cd ..\DesignBuildFly\python_tools\data\windTunnel
-git add .
-git commit -m "script upload"
-git push origin
-cd "..\..\..\..\AP 23-24"
